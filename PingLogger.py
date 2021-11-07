0cf427bde3da153dd3431e54cbb870acce18d253
Javid Guliyev <31576058+cavidqlyv@users.noreply.github.com>
2021-11-07 22:36:11 +0400

    added new info types

--- PingLogger.py
+++ PingLogger.py
@@ -20,20 +20,32 @@ def maxping():
     if "Maximum" in output:
         maxping=int(output.split()[output.split().index("Maximum")+2][:2])
         #Detailed information about the ping
-        print(' '.join([str(elem) for elem in packet_info])[:-1], "Maximum Ping :", maxping)
+        #print(' '.join([str(elem) for elem in packet_info])[:-1], "Maximum Ping :", maxping)
     else:
         maxping=0
     
     #Categorize the ping information
     if "Destination host unreachable." in output:
         return "Destination host unreachable;" +str(maxping) + "ms;" + str(time.ctime())+"\n"
+    
+    elif "General failure."  in output:
+        return "General failure;" +str(maxping) + "ms;" + str(time.ctime())+"\n"
+    
+    elif "Request timed out." in output:
+        return "Request timed out;" +str(maxping) + "ms;" + str(time.ctime())+"\n"
+
     elif "0% loss" not in output:
         percent_of_lost_packets=packet_info[10].split("(")[1]
         return percent_of_lost_packets+ " loss;" + str(maxping) + "ms;" + str(time.ctime())+"\n"
+
     elif maxping>250:
         return "high ping;"+ str(maxping) + "ms;" + str(time.ctime())+"\n"
+
     elif "time=" in output:
         return 0
+
+    else:
+        return output
 
 def write_to_file(info,filename="ping.csv"):
     """This file writes information to the created file"""
